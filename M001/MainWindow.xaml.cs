187a085390e5a15609b525ebdaabd329395603a9
ppedvLukasK <lukask@ppedv.de>
2023-11-02 10:00:33 +0100

    Initial commit

---
+++ M001/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace M001;
+
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent(); //Baut den UI (C#) Code aus dem XAML-Code
+		//Sollte immer als erstes im Konstruktor stehen
+
+		TestTextBlock.Text = "ABC";
+		TestTextBlock.FontSize = 100;
+	}
+}
---
+++ M001_WinForms/Form1.cs
@@ -0,0 +1,10 @@
+namespace M001_WinForms
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+	}
+}
