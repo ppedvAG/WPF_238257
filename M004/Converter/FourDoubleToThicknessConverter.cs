78f92f3535f94c60ad90ea1b015683be008596e3
ppedvLukasK <lukask@ppedv.de>
2023-11-03 12:25:38 +0100

    M004

---
+++ M004/Converter/FourDoubleToThicknessConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace M004.Converter;
+
+class FourDoubleToThicknessConverter : IMultiValueConverter
+{
+	public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+	{
+		//values ist jetzt ein Array
+		double[] d = values.OfType<double>().ToArray();
+		return new Thickness(d[0], d[1], d[2], d[3]);
+	}
+
+	public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
---
+++ M004/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace M004;
+
+public partial class MainWindow : Window
+{
+	public int Zaehler
+	{
+		get { return (int) GetValue(ZaehlerProperty); }
+		set { SetValue(ZaehlerProperty, value); }
+	}
+
+	public static readonly DependencyProperty ZaehlerProperty =
+		DependencyProperty.Register
+		(
+			"Zaehler", //Namen des Properties
+			typeof(int), //Type des Properties
+			typeof(MainWindow), //Typ der Klasse in dem das Property enthalten ist
+			new PropertyMetadata(20) //Standardwert für das Property festlegen (optional)
+		);
+
+	public MainWindow()
+	{
+		InitializeComponent();
+		this.DataContext = this; //Hier den DataContext auf das Fenster selbst setzen
+
+		//BindingExpression exp = BindingSlider.GetBindingExpression(TextBox.TextProperty);
+		//exp.UpdateTarget();  //Expliziter Befehl zum Updaten
+	}
+
+	private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+	{
+		Zaehler = (int) e.NewValue; //Hier wird nur der Wert von dem Slider in das Zaehler Property hineingeschrieben
+		//Kein UI Update
+	}
+}
