72ada0dba1d69dcace31c206f36388aece078cfc
ppedvLukasK <lukask@ppedv.de>
2023-11-06 12:59:44 +0100

    M006

---
+++ M006/DoubleToThicknessConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace M006;
+
+public class DoubleToThicknessConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		//Source -> Target
+		//Slider -> Margin
+		//Beim Übertragen des Wertes via Binding wird der Converter befragt, wie die Konvertierung erfolgen soll
+		double d = (double)value;
+		Thickness t = new Thickness(d);
+		return t;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		//Target -> Source
+		//Margin -> Slider (nicht möglich)
+		return 0;
+	}
+}
---
+++ M006/FourValueToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace M006
+{
+	internal class FourValueToColorConverter : IMultiValueConverter
+	{
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			byte[] c = values
+				.OfType<double>()
+				.Select(e => (byte) e)
+				.ToArray();
+			return new SolidColorBrush(Color.FromArgb(c[3], c[0], c[1], c[2]));
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => throw new NotImplementedException();
+	}
+}
