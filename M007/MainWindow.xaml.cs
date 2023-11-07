db17fb3cc2329a685c719c3059f6dbb2d53b0f4b
ppedvLukasK <lukask@ppedv.de>
2023-11-07 11:16:39 +0100

    M007

--- M007/MainWindow.xaml.cs
+++ M007/MainWindow.xaml.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.ObjectModel;
-using System.Linq;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows;
 
 namespace M007;
@@ -16,6 +16,10 @@ public partial class MainWindow : Window
 	public MainWindow()
 	{
 		InitializeComponent();
+
+		//Hierüber den String für das Einbinden eines Enums in dem XAML holen
+		//System.Private.CoreLib, Version=7.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e
+		Type t = typeof(DayOfWeek);
 	}
 
 	private void ControlTemplate_Click(object sender, RoutedEventArgs e)
